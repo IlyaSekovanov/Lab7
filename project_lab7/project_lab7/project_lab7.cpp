4cbb674be44bc0f758b5db607a976d6c6a529d3a
VladickShestakov <v.shestakov@uniyar.ac.ru>
2022-12-20 20:24:19 +0300

    Add HeapSort

--- project_lab7/project_lab7/project_lab7.cpp
+++ project_lab7/project_lab7/project_lab7.cpp
@@ -2,7 +2,15 @@
 #include <vector>
 #include <chrono>
 #include <ctime>
-void mergeSort(std::vector<long int>& s, size_t start, size_t end) {
+#include <algorithm>
+using namespace std;
+void outVector(std::vector<long int>& s, int n) //Вывод вектора (Если нужно)
+{
+	for (int i = 0; i < n; ++i) {
+				cout << s[i] << endl; 
+			}
+}
+void mergeSort(std::vector<long int>& s, size_t start, size_t end) { //Сортировка слиянием (Илья)
 	if (end - start < 2) {
 		return;
 	}
@@ -32,14 +40,63 @@ void mergeSort(std::vector<long int>& s, size_t start, size_t end) {
 		s[i] = b[i - start];
 	}
 }
-int main() {
-	srand(time(0));
-	std::vector<long int> s(10000000);
-	for (int i = 0; i < 10000000; ++i) {
-		s[i] = rand();
+
+void heapify(vector <long int> &arr, int n, int i) // функция для пирамидальной
+{
+	int largest = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+	if (l < n && arr[l] > arr[largest])
+		largest = l;
+	if (r < n && arr[r] > arr[largest])
+		largest = r;
+	if (largest != i)
+	{
+		swap(arr[i], arr[largest]);
+		heapify(arr, n, largest);
 	}
-	auto start = std::chrono::steady_clock::now();
-	mergeSort(s, 0, s.size());
-	auto end = std::chrono::steady_clock::now();
-	std::cout << "Elapsed time in milliseconds: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+}
+
+void heapsort(vector <long int> arr, int n) //Пирамидальная (Влад)
+{
+		for (int i = n / 2 - 1; i >= 0; i--)
+			heapify(arr, n, i);
+		for (int i = n - 1; i >= 0; i--)
+		{
+			swap(arr[0], arr[i]);
+			heapify(arr, i, 0);
+		}
+}
+
+int main() {
+	int t = 1000; // Количество тестов
+	cout << "MergeSort HeapSort ...other" << endl; //Названия писать сюда
+	while (--t)
+	{
+		srand(time(0));
+		int n = 10000000; // Количество элементов
+		std::vector<long int> s(n);
+		for (int i = 0; i < n; ++i) {
+			s[i] = rand();
+		}
+
+		auto start = std::chrono::steady_clock::now();
+		mergeSort(s, 0, s.size()); //Сортировка слиянием (Илья)
+		auto end = std::chrono::steady_clock::now();
+
+		std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << "      ";
+
+		//outVector(s, n); //Вывод вектора (Если нужно)
+
+		for (int i = 0; i < n; ++i) {
+			s[i] = rand(); //Переопределение вектора рандомными числами
+		}
+
+		start = std::chrono::steady_clock::now();
+		heapsort(s, n); //Пирамидальная (Влад)
+		end = std::chrono::steady_clock::now();
+
+		std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << "      ";
+		cout << endl;
+	}
 }
